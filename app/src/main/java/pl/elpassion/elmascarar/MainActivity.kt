e2d8d4c3bbdd4b11d439da6fcefe74edb361c954
rafal <trzcinskiraf@gmail.com>
2016-02-04 12:42:00 +0100

    add phoenixframework dependency

--- app/src/main/java/pl/elpassion/elmascarar/MainActivity.kt
+++ app/src/main/java/pl/elpassion/elmascarar/MainActivity.kt
@@ -1,18 +1,15 @@
 package pl.elpassion.elmascarar
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.view.View
-import android.view.Menu
-import android.view.MenuItem
+import org.phoenixframework.channels.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val socket = Socket("").connect()
     }
 }
