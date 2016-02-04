e89e4eee386c18c2f76741ad082f6b68f77401db
jasiekpor <jasiekpor@gmail.com>
2016-02-04 12:02:54 +0100

    Clean layouts, changed emulator version to 21 in .travis.yml

--- scripts/emulator.sh
+++ scripts/emulator.sh
@@ -1,4 +1,4 @@
-echo no | android create avd --force -n test -t android-16 --abi armeabi-v7a
+echo no | android create avd --force -n test -t android-21 --abi armeabi-v7a
 emulator -avd test -no-skin -no-audio -no-window &
 android-wait-for-emulator
 sleep 30
