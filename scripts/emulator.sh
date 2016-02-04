2480762b86dfe6c747d530948d371be139eb052e
jasiekpor <jasiekpor@gmail.com>
2016-02-04 11:48:32 +0100

    Travis integration

---
+++ scripts/emulator.sh
@@ -0,0 +1,5 @@
+echo no | android create avd --force -n test -t android-16 --abi armeabi-v7a
+emulator -avd test -no-skin -no-audio -no-window &
+android-wait-for-emulator
+sleep 30
+adb shell input keyevent 82
