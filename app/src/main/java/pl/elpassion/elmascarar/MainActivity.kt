1856f1a1340c4c8e04fd0a3fa1c22f6619c82b95
dmalantowicz <dominik.malantowicz@gmail.com>
2016-02-04 15:41:51 +0100

    internet permission, connection to channel

--- app/src/main/java/pl/elpassion/elmascarar/MainActivity.kt
+++ app/src/main/java/pl/elpassion/elmascarar/MainActivity.kt
@@ -2,7 +2,8 @@ package pl.elpassion.elmascarar
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import org.phoenixframework.channels.*
+import android.util.Log
+import org.phoenixframework.channels.Socket
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val socket = Socket("").connect()
+        val socket = Socket("ws://el-mascarar.herokuapp.com/socket/websocket")
+        socket.connect()
+        val channel = socket.chan("games:lobby", null)
+
+        channel.on("shout", { envelope -> Log.e("MESSAGE SHOUT: ", envelope.toString()) })
+
+        channel.join().receive("ok", { envelope ->
+            Log.e("MESSAGE OK: ", envelope.toString())
+            channel.push("shout", null)
+        }).receive("ignore", { envelope -> Log.e("MESSAGE IGNORE: ", envelope.toString()) })
+
     }
 }
