802fcd6e47a77d7db5b02e4827753563be9f994a
Karol Kowalski <ordon1313@gmail.com>
2016-04-26 00:07:25 +0200

    Continuous Bot

--- bot/src/main/java/pl/elpassion/elabyrinth/bot/Bot.kt
+++ bot/src/main/java/pl/elpassion/elabyrinth/bot/Bot.kt
@@ -1,25 +1,36 @@
 package pl.elpassion.elabyrinth.bot
 
+import pl.elpassion.elabyrinth.core.Cell
 import pl.elpassion.elabyrinth.core.Direction
 import pl.elpassion.elabyrinth.core.Game
 import pl.elpassion.elabyrinth.core.LabyrinthSocket
+import java.util.*
 
 object Bot {
 
+    val random = Random()
     val socket by lazy { LabyrinthSocket() }
-    lateinit var game: Game
+    var map: List<List<Cell>>? = null
 
     @JvmStatic fun main(args: Array<String>) {
-        socket.onGame { game = it }
-        Thread {
-            Thread.sleep(5000)
-            walkThrough(Board(game.map).solve())
-        }.start()
+        socket.onGame(onGame)
     }
+
+    val onGame: (Game) -> Unit = {
+        if (map != it.map) {
+            map = it.map
+            solveAsync(it.map)
+        }
+    }
+
+    private fun solveAsync(map: List<List<Cell>>) {
+        Thread { walkThrough(Board(map).solve()) }.start()
+    }
+
 
     val walkThrough: (List<Direction>) -> Unit = {
         it.forEach {
-            Thread.sleep(1000)
+            Thread.sleep(500L + random.nextInt(500))
             socket.move(it)
         }
     }
