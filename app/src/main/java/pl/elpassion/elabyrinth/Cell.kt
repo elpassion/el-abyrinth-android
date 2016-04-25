85f912895f0f6a7a3c6055dce28865c26ff04cec
Karol Kowalski <ordon1313@gmail.com>
2016-04-25 13:28:35 +0200

    drawables

--- app/src/main/java/pl/elpassion/elabyrinth/Cell.kt
+++ app/src/main/java/pl/elpassion/elabyrinth/Cell.kt
@@ -4,7 +4,7 @@ import android.support.annotation.LayoutRes
 import pl.elpassion.elmascarar.R
 
 enum class Cell(val value: Int, @LayoutRes val layoutRes: Int) {
-    FREE(0, R.layout.free), WALL(1, R.layout.wall), START(2, R.layout.free), END(3, R.layout.end);
+    FREE(0, R.layout.free), WALL(1, R.layout.wall), START(2, R.layout.start), END(3, R.layout.end);
 
     companion object {
         fun fromInt(value: Int): Cell {
